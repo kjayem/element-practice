34bd49ec628d37e840fcaab51c790fbd165a137a
kjayem <flwfeeld@gmail.com>
2021-08-12 17:42:29 +0900

    created homepage with element

--- main.js
+++ main.js
@@ -1,9 +1,26 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+var fs = require('fs');
+var template = require('./lib/template.js');
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+// app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', function(request, response) {
+	fs.readdir('./data', function(error, filelist){
+    var title = 'Welcome';
+    var description = 'Hello, Node.js';
+    var list = template.list(filelist);
+    var html = template.HTML(title, list,
+      `<h2>${title}</h2>${description}`,
+      `<a href="/create">create</a>`
+    );
+    response.send(html);
+  });
+});
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+});
 
 // var http = require('http');
 // var fs = require('fs');
