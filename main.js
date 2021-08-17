3ef59bdf8ec4ef24f4c9750c8d3d79d73d8178b3
kjayem <flwfeeld@gmail.com>
2021-08-17 19:43:54 +0900

    separated routes to its own file using router

--- main.js
+++ main.js
@@ -3,11 +3,10 @@ const express = require('express');
 const app = express();
 var fs = require('fs');
 var qs = require('querystring');
-var path = require('path');
 var bodyParser = require('body-parser');
 var compression = require('compression');
-var sanitizeHtml = require('sanitize-html');
 var template = require('./lib/template.js');
+var topicRouter = require('./routes/topic');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended: false}));
@@ -18,6 +17,8 @@ app.get('*', function(request, response, next) {
     next();
   });
 });
+
+app.use('/topic', topicRouter);
 
 //홈페이지
 // app.get('/', (req, res) => res.send('Hello World!'))
@@ -33,108 +34,7 @@ app.get('/', function(request, response) {
     response.send(html);
 });
 
-//페이지 생성 클릭했을때
-app.get('/topic/create', function(request, response) {
-  var title = 'WEB - create';
-  var list = template.list(request.list);
-  var html = template.HTML(title, list, `
-    <form action="/topic/create_process" method="post">
-      <p><input type="text" name="title" placeholder="title"></p>
-      <p>
-        <textarea name="description" placeholder="description"></textarea>
-      </p>
-      <p>
-        <input type="submit">
-      </p>
-    </form>
-  `, '');
-  response.send(html);
-});
 
-//페이지 생성하고 submit 클릭했을때
-app.post('/topic/create_process', function(request, response) {
-  var post = request.body;
-  var title = post.title;
-  var description = post.description;
-  fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-    response.redirect(`/topic/${title}`);
-  })
-});
-
-//페이지 수정 클릭했을때
-app.get('/topic/update/:pageId', function(request, response) {
-  var filteredId = path.parse(request.params.pageId).base;
-  fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-    var title = request.params.pageId;
-    var list = template.list(request.list);
-    var html = template.HTML(title, list,
-      `
-      <form action="/topic/update_process" method="post">
-        <input type="hidden" name="id" value="${title}">
-        <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-        <p>
-          <textarea name="description" placeholder="description">${description}</textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-      `,
-      `<a href="/topic/create">create</a> <a href="/topic/update/${title}">update</a>`
-    );
-    response.send(html);
-  });
-});
-
-//페이지 수정하고 submit 클릭했을때
-app.post('/topic/update_process', function(request, response) {
-  var post = request.body;
-  var id = post.id;
-  var title = post.title;
-  var description = post.description;
-  fs.rename(`data/${id}`, `data/${title}`, function(error){
-    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-      response.redirect(`/topic/${title}`);
-    })
-  });
-});
-
-//페이지 삭제 눌렀을때
-app.post('/topic/delete_process', function(request, response) {
-  var post = request.body;
-  var id = post.id;
-  var filteredId = path.parse(id).base;
-  fs.unlink(`data/${filteredId}`, function(error){
-    response.redirect('/');
-  });
-});
-
-//상세페이지 눌렀을때 (HTML, CSS, etc.)
-app.get('/topic/:pageId', function(request, response, next) {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      if(err) {
-        next(err);
-      } else {
-        var title = request.params.pageId;
-        var sanitizedTitle = sanitizeHtml(title);
-        var sanitizedDescription = sanitizeHtml(description, {
-          allowedTags:['h1']
-        });
-        var list = template.list(request.list);
-        var html = template.HTML(sanitizedTitle, list,
-          `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-          ` <a href="/topic/create">create</a>
-            <a href="/topic/update/${sanitizedTitle}">update</a>
-            <form action="/topic/delete_process" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`
-        );
-        response.send(html);
-      }
-    });
-});
 
 app.use(function(req, res, next) {
   res.status(404).send('Sorry cant find that!')
